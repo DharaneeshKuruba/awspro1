a6d652e0341f1277c742c95591aecb7ddf62b926
Dharaneesh <dharaneeshkuruba.2005@gmail.com>
2026-02-24 10:55:00 +0530

    Initial commit

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_USER = "root"
-DB_PASSWORD = "jaya@123"
+DB_PASSWORD = "root123"
 DB_HOST = "localhost"
 DB_PORT = "3306"
 DB_NAME = "project1"
